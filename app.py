51f58bc1595fcb1d1b355fa00b3c5ad54fc68e53
Sohamsanghavi <96059983+Sohamsanghavi@users.noreply.github.com>
2023-11-04 17:28:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask,request,render_template
 from datetime import date
-import pickle
+import subprocess
 import pandas as pd
 import joblib
 import xgboost as xgb
 import numpy as np
+import knnclass
+import knnstop
+from sklearn.impute import KNNImputer
 
-model = joblib.load('flight_pred.pkl')
+
+model = joblib.load('C:/Users/sangh/Downloads/Comding/BTP2/Prediction/x/flight_xgb.pkl')
+
 
 app = Flask(__name__)
 @app.route('/')
@@ -14,8 +19,21 @@ app = Flask(__name__)
 def home():
 	return render_template('home.html')
 
+def classify_hour(hour):
+    if 6 <= hour <= 12:
+        return 1, 0, 0
+    elif hour >= 18:
+        return 0, 1, 0
+    elif hour <6:
+        return 0, 0, 1
+    else:
+        return 0, 0, 0
+
+
 @app.route('/predict',methods=['GET','POST'])
 # @cross_origin()
+
+
 def predict():
     if request.method=='POST':
         dep_time = request.form['Dep_Time']
@@ -27,202 +45,141 @@ def predict():
         Departure_hour = pd.to_datetime(dep_time,format="%Y-%m-%dT%H:%M").hour
         Departure_min = pd.to_datetime(dep_time,format="%Y-%m-%dT%H:%M").minute 
 
-
-        if Departure_hour>=6 and Departure_hour<=12:
-            d6_12=1
-            da6=0
-            db6=0
-        
-        elif Departure_hour>=18 :
-            d6_12=0
-            da6=1
-            db6=0
-
-        elif Departure_hour<6:
-            d6_12=0
-            da6=0
-            db6=1
-        
-        else:
-            d6_12=0
-            da6=0
-            db6=0
-
         today_day=date.today().day
         Days_left=Journey_day-today_day
-
         Arrival_hour =  pd.to_datetime(arrival_time,format="%Y-%m-%dT%H:%M").hour
         Arrival_min =  pd.to_datetime(arrival_time,format="%Y-%m-%dT%H:%M").minute
 
-        if Arrival_hour>=6 and Arrival_hour<=12:
-            a6_12=1
-            aa6=0
-            ab6=0
-        
-        elif Arrival_hour>=18 :
-            a6_12=0
-            aa6=1
-            ab6=0
-
-        elif Arrival_hour<6:
-            a6_12=0
-            aa6=0
-            ab6=1
-        
-        else:
-            a6_12=0
-            aa6=0
-            ab6=0
+        d6_12, da6, db6 = classify_hour(Departure_hour)
+        a6_12, aa6, ab6 = classify_hour(Arrival_hour)
 
         Total_stops= int(request.form['stops'])
         Class=int(request.form['Class'])
 
+
         dur_hour = abs(Arrival_hour-Departure_hour)
         dur_min = abs(Arrival_min-Departure_min)
 
-        Source=request.form["Source"]
+        Source = request.form["Source"]
+        source_mapping = {
+            'Delhi': (1, 0, 0, 0, 0, 0),
+            'Kolkata': (0, 1, 0, 0, 0, 0),
+            'Mumbai': (0, 0, 1, 0, 0, 0),
+            'Chennai': (0, 0, 0, 1, 0, 0),
+            'Hyderabad': (0, 0, 0, 0, 1, 0),
+            'Bangalore': (0, 0, 0, 0, 0, 1),
+            'Ahemdabad':(0, 0, 0, 0, 0, 0)
+        }
 
-        if (Source == 'Delhi'):
-            Source_Delhi = 1
-            Source_Kolkata = 0
-            Source_Mumbai = 0
-            Source_Chennai = 0
-            Source_Banglore =0
-            Source_Hyderabad =0
+        source_values = source_mapping.get(Source)
 
-        elif (Source == 'Kolkata'):
-            Source_Delhi = 0
-            Source_Kolkata = 1
-            Source_Mumbai = 0
-            Source_Chennai = 0
-            Source_Banglore =0
-            Source_Hyderabad =0
+        Source_Delhi, Source_Kolkata, Source_Mumbai, Source_Chennai, Source_Hyderabad, Source_Banglore = source_values
 
-        elif (Source == 'Mumbai'):
-            Source_Delhi = 0
-            Source_Kolkata = 0
-            Source_Mumbai = 1
-            Source_Chennai = 0
-            Source_Banglore =0
-            Source_Hyderabad =0
-
-        elif (Source == 'Chennai'):
-            Source_Delhi = 0
-            Source_Kolkata = 0
-            Source_Mumbai = 0
-            Source_Chennai = 1
-            Source_Banglore =0
-            Source_Hyderabad =0
-
-        elif (Source == 'Hydrabad'):
-            Source_Delhi = 0
-            Source_Kolkata = 0
-            Source_Mumbai = 0
-            Source_Chennai = 0
-            Source_Banglore =0
-            Source_Hyderabad =0
-
-        elif (Source == 'Bangalore'):
-            Source_Delhi = 0
-            Source_Kolkata = 0
-            Source_Mumbai = 0
-            Source_Chennai = 0
-            Source_Banglore =1
-            Source_Hyderabad =0
-        
-        else:
-            Source_Delhi = 0
-            Source_Kolkata = 0
-            Source_Mumbai = 0
-            Source_Chennai = 0
-            Source_Banglore =0
-            Source_Hyderabad =1
-
-
-        # Destination_Bangalore	Destination_Chennai	Destination_Delhi	Destination_Hyderabad	Destination_Kolkata	Destination_Mumbai
         Destination = request.form["Destination"]
+        destination_mapping = {
+            'Bangalore': (1, 0, 0, 0, 0, 0),
+            'Delhi': (0, 1, 0, 0, 0, 0),
+            'Hyderabad': (0, 0, 1, 0, 0, 0),
+            'Kolkata': (0, 0, 0, 1, 0, 0),
+            'Mumbai': (0, 0, 0, 0, 1, 0),
+            'Chennai':(0, 0, 0, 0, 0, 1),
+            'Ahemdabad':(0, 0, 0, 0, 0, 0)
+        }
 
-        if (Destination == 'Bangalore'):
-            Destination_Bangalore = 1
-            Destination_Delhi = 0
-            Destination_Hyderabad = 0
-            Destination_Kolkata = 0
-            Destination_Mumbai = 0
-            Destination_Chennai = 0
+        destination_values = destination_mapping.get(Destination)
+
+        Destination_Bangalore, Destination_Delhi, Destination_Hyderabad, Destination_Kolkata, Destination_Mumbai, Destination_Chennai = destination_values
         
-        elif (Destination == 'Delhi'):
-            Destination_Bangalore = 0
-            Destination_Delhi = 1
-            Destination_Hyderabad = 0
-            Destination_Kolkata = 0
-            Destination_Mumbai = 0
-            Destination_Chennai = 0
-
-        elif (Destination == 'Hyderabad'):
-            Destination_Bangalore = 0
-            Destination_Delhi = 0
-            Destination_Hyderabad = 1
-            Destination_Kolkata = 0
-            Destination_Mumbai = 0
-            Destination_Chennai = 0
-
-        elif (Destination == 'Kolkata'):
-            Destination_Bangalore = 0
-            Destination_Delhi = 0
-            Destination_Hyderabad = 0
-            Destination_Kolkata = 1
-            Destination_Mumbai = 0
-            Destination_Chennai = 0
-
-        elif (Destination =='Mumbai'):
-            Destination_Bangalore = 0
-            Destination_Delhi = 0
-            Destination_Hyderabad = 0
-            Destination_Kolkata = 0
-            Destination_Mumbai = 1
-            Destination_Chennai = 0
-        
+        if Class==-1:
+            inp1=knnclass.knnclass1({
+            'Journey_day': Journey_day,
+            'Class': np.nan,
+            'Total_stops': Total_stops,
+            'Duration_in_hours': dur_hour,
+            'Days_left': Days_left,
+            'Journey_month': Journey_month,
+            '6 AM - 12 PM': a6_12,
+            'After 6 PM': aa6,
+            'Before 6 AM': ab6,
+            'Departure_6 AM - 12 PM': d6_12,
+            'Departure_After 6 PM': da6,
+            'Departure_Before 6 AM': db6,
+            'Source_Bangalore': Source_Banglore,
+            'Source_Chennai': Source_Chennai,
+            'Source_Delhi': Source_Delhi,
+            'Source_Hyderabad': Source_Hyderabad,
+            'Source_Kolkata': Source_Kolkata,
+            'Source_Mumbai': Source_Mumbai,
+            'Destination_Bangalore': Destination_Bangalore,
+            'Destination_Chennai': Destination_Chennai,
+            'Destination_Delhi': Destination_Delhi,
+            'Destination_Hyderabad': Destination_Hyderabad,
+            'Destination_Kolkata': Destination_Kolkata,
+            'Destination_Mumbai': Destination_Mumbai
+            })
+            inp = pd.DataFrame({key: [value] for key, value in inp1.items()})
+        elif Total_stops==-1:
+            inp1=knnclass.knnclass1({
+            'Journey_day': Journey_day,
+            'Class': Class,
+            'Total_stops': np.nan,
+            'Duration_in_hours': dur_hour,
+            'Days_left': Days_left,
+            'Journey_month': Journey_month,
+            '6 AM - 12 PM': a6_12,
+            'After 6 PM': aa6,
+            'Before 6 AM': ab6,
+            'Departure_6 AM - 12 PM': d6_12,
+            'Departure_After 6 PM': da6,
+            'Departure_Before 6 AM': db6,
+            'Source_Bangalore': Source_Banglore,
+            'Source_Chennai': Source_Chennai,
+            'Source_Delhi': Source_Delhi,
+            'Source_Hyderabad': Source_Hyderabad,
+            'Source_Kolkata': Source_Kolkata,
+            'Source_Mumbai': Source_Mumbai,
+            'Destination_Bangalore': Destination_Bangalore,
+            'Destination_Chennai': Destination_Chennai,
+            'Destination_Delhi': Destination_Delhi,
+            'Destination_Hyderabad': Destination_Hyderabad,
+            'Destination_Kolkata': Destination_Kolkata,
+            'Destination_Mumbai': Destination_Mumbai
+            })
+            inp = pd.DataFrame({key: [value] for key, value in inp1.items()})
         else:
-            Destination_Bangalore = 0
-            Destination_Delhi = 0
-            Destination_Hyderabad = 0
-            Destination_Kolkata = 0
-            Destination_Mumbai = 0
-            Destination_Chennai = 1
+            inp = pd.DataFrame({
+                'Journey_day': [Journey_day],
+                'Class': [Class],
+                'Total_stops': [Total_stops],
+                'Duration_in_hours': [dur_hour],
+                'Days_left': [Days_left],
+                'Journey_month': [Journey_month],
+                '6 AM - 12 PM': [a6_12],
+                'After 6 PM': [aa6],
+                'Before 6 AM': [ab6],
+                'Departure_6 AM - 12 PM': [d6_12],
+                'Departure_After 6 PM': [da6],
+                'Departure_Before 6 AM': [db6],
+                'Source_Bangalore': [Source_Banglore],
+                'Source_Chennai': [Source_Chennai],
+                'Source_Delhi': [Source_Delhi],
+                'Source_Hyderabad': [Source_Hyderabad],
+                'Source_Kolkata': [Source_Kolkata],
+                'Source_Mumbai': [Source_Mumbai],
+                'Destination_Bangalore': [Destination_Bangalore],
+                'Destination_Chennai': [Destination_Chennai],
+                'Destination_Delhi': [Destination_Delhi],
+                'Destination_Hyderabad': [Destination_Hyderabad],
+                'Destination_Kolkata': [Destination_Kolkata],
+                'Destination_Mumbai': [Destination_Mumbai]
+            })
 
-        inp = pd.DataFrame({
-            'Journey_day': [Journey_day],
-            'Class': [Class],
-            'Total_stops': [Total_stops],
-            'Duration_in_hours': [dur_hour],
-            'Days_left': [Days_left],
-            'Journey_month': [Journey_month],
-            '6 AM - 12 PM': [a6_12],
-            'After 6 PM': [aa6],
-            'Before 6 AM': [ab6],
-            'Departure_6 AM - 12 PM': [d6_12],
-            'Departure_After 6 PM': [da6],
-            'Departure_Before 6 AM': [db6],
-            'Source_Bangalore': [Source_Banglore],
-            'Source_Chennai': [Source_Chennai],
-            'Source_Delhi': [Source_Delhi],
-            'Source_Hyderabad': [Source_Hyderabad],
-            'Source_Kolkata': [Source_Kolkata],
-            'Source_Mumbai': [Source_Mumbai],
-            'Destination_Bangalore': [Destination_Bangalore],
-            'Destination_Chennai': [Destination_Chennai],
-            'Destination_Delhi': [Destination_Delhi],
-            'Destination_Hyderabad': [Destination_Hyderabad],
-            'Destination_Kolkata': [Destination_Kolkata],
-            'Destination_Mumbai': [Destination_Mumbai]
-        })
-
-        data = xgb.DMatrix(inp)
+        data = xgb.DMatrix(inp,enable_categorical=True)
 
         output = model.predict(data)
 
-        output = round(float(output[0]), 2)
-        return render_template('home.html', predictions='You will have to Pay approx Rs. {}'.format(output))
+        output1 = round(float(output[0]),2)
+        return render_template('home.html',predictions='You will have to Pay approx Rs. {}'.format(output1))
 
 
 if __name__ == '__main__':
